923d20ab036f668378ac20d1bd239e40fc22c46e
Firat Tale <firat.tale@auto1.com>
2021-11-07 20:16:13 +0100

    Adding relevant htmls

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import ItemManagerContract from "./contracts/ItemManager.json";
+import ItemContract from "./contracts/Item.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-	state = { storageValue: 0, web3: null, accounts: null, contract: null };
+	state = {
+		storageValue: 0,
+		web3: null,
+		accounts: null,
+		itemManagerContract: null,
+		itemContract: null,
+		cost: 0,
+		itemName: "example1",
+	};
 
 	componentDidMount = async () => {
 		try {
@@ -17,12 +26,19 @@ class App extends Component {
 
 			// Get the contract instance.
 			const networkId = await web3.eth.net.getId();
-			const deployedNetwork = SimpleStorageContract.networks[networkId];
-			const instance = new web3.eth.Contract(SimpleStorageContract.abi, deployedNetwork && deployedNetwork.address);
+			const itemManager = new web3.eth.Contract(
+				ItemManagerContract.abi,
+				ItemManagerContract.networks[networkId] && ItemManagerContract.networks[networkId].address
+			);
+			const item = new web3.eth.Contract(
+				ItemContract.abi,
+				ItemContract.networks[networkId] && ItemContract.networks[networkId].address
+			);
 
 			// Set web3, accounts, and contract to the state, and then proceed with an
 			// example of interacting with the contract's methods.
-			this.setState({ web3, accounts, contract: instance }, this.runExample);
+			this.setState({ web3, accounts, itemManagerContract: itemManager, itemContract: item });
+			this.listenToPaymentEvent();
 		} catch (error) {
 			// Catch any errors for any of the above operations.
 			alert(`Failed to load web3, accounts, or contract. Check console for details.`);
@@ -30,33 +46,55 @@ class App extends Component {
 		}
 	};
 
-	runExample = async () => {
-		const { accounts, contract } = this.state;
-
-		// Stores a given value, 5 by default.
-		await contract.methods.set(50).send({ from: accounts[0] });
-
-		// Get the value from the contract to prove it worked.
-		const response = await contract.methods.get().call();
-
-		// Update state with the result.
-		this.setState({ storageValue: response });
+	listenToPaymentEvent = () => {
+		this.state.itemManagerContract.events.SupplyChainStep().on("data", async (event) => {
+			console.log(event);
+			let itemObj = await this.state.itemManagerContract.methods.items(event.returnValues._itemIndex).call();
+			console.log("itemObj", itemObj);
+		});
 	};
 
+	handleSubmit = async () => {
+		const { accounts, itemManagerContract } = this.state;
+		const { itemName, cost } = this.state;
+		const result = await itemManagerContract.methods.createItem(itemName, cost).send({ from: accounts[0] });
+		console.log(`result`, result);
+	};
 	render() {
 		if (!this.state.web3) {
 			return <div>Loading Web3, accounts, and contract...</div>;
 		}
 		return (
 			<div className="App">
-				<h1>Good to Go!</h1>
-				<p>Your Truffle Box is installed and ready.</p>
-				<h2>Smart Contract Example</h2>
-				<p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-				<p>
-					Try changing the value stored on <strong>line 42</strong> of App.js.
-				</p>
-				<div>The stored value is: {this.state.storageValue}</div>
+				<h1>Event Trigger / Supply Chain Example</h1>
+				<h2>Items</h2>
+				<h2>Add Items</h2>
+				Cost in Wei:{" "}
+				<input
+					type="text"
+					name="cost"
+					value={this.state.cost}
+					onChange={(e) => {
+						this.setState({
+							cost: e.target.value,
+						});
+					}}
+				/>
+				Item Id:{" "}
+				<input
+					type="text"
+					name="itemName"
+					value={this.state.itemName}
+					onChange={(e) => {
+						this.setState({
+							itemName: e.target.value,
+						});
+					}}
+				/>
+				<button type="button" onClick={this.handleSubmit}>
+					{" "}
+					Create new Item
+				</button>
 			</div>
 		);
 	}
